1faa6fd215afd56d5826ee5ec61ccf63ab30b737
Shakil Almirr <101876451+ShakilM26@users.noreply.github.com>
2022-09-18 18:06:18 +0600

    Update code.sql

--- price hike/code.sql
+++ price hike/code.sql
@@ -6,3 +6,9 @@ COMMIT;
 ALTER TABLE market_price RENAME COLUMN price to current_price;
 commit;
 END;
+
+
+-- Find price of beef when it's lower than 500 taka  
+
+SELECT * FROM market_price WHERE current_price in (SELECT DISTINCT(current_price) FROM market_price where 
+                                     current_price < 500 and items = 'Beef');

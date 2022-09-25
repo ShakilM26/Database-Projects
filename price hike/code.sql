30dec9f2176952dd97203e844788725654c2bd6f
Shakil Almirr <101876451+ShakilM26@users.noreply.github.com>
2022-09-25 16:04:39 +0600

    Update code.sql

--- price hike/code.sql
+++ price hike/code.sql
@@ -8,7 +8,25 @@ commit;
 END;
 
 
--- Find price of beef when it's lower than 500 taka  
 
-SELECT * FROM market_price WHERE current_price in (SELECT DISTINCT(current_price) FROM market_price where 
-                                     current_price < 500 and items = 'Beef');
+-- Find price of Rice when it's lower than 55
+SELECT * FROM market_price where items = 'Rice' and current_price <= 55; 
+
+
+
+-- There are some duplicate values. I want to print how many times there are duplicate values
+SELECT items, current_price, COUNT(*)
+FROM market_price where (items = 'Rice' and current_price <= 55)  
+GROUP BY items, current_price;
+
+
+
+-- Now I want to print how many times all items have duplicate values.
+SELECT items, current_price, COUNT(*)
+FROM market_price   
+GROUP BY items, current_price having count(*) > 1 order by items desc;
+
+
+
+-- Find price of beef when it's lower than 500 taka 
+

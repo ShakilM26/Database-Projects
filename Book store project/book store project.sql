0d22cfed5c5855a76cdc4fd20246e11de261c276
Shakil Almirr <101876451+ShakilM26@users.noreply.github.com>
2022-09-26 20:09:32 +0600

    Update book store project.sql

--- Book store project/book store project.sql
+++ Book store project/book store project.sql
@@ -31,3 +31,6 @@ select book_name, author from vertical_part offset 5 rows fetch first 5 row only
 select author, sum(price) from vertical_part group by author having sum(price) >= 1000 order by sum(price) desc fetch first 5 row only;
 
 
+select author, book_name, min(year) from book_store group by year, author,book_name order by year asc;
+-- we can get rid of the above query when we using least function
+select book_name, least(year) from book_store order by year asc;
